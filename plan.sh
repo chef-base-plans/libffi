f8ad8424d34fccf8af0b8a646fa46d0d980e8ef4
Nimit <nimit.jyotiana@hotmail.com>
2021-10-27 11:07:13 +0530

    upgraded version to v3.4.2
    Signed-off-by: Nimit <nimit.jyotiana@hotmail.com>

--- plan.sh
+++ plan.sh
@@ -1,16 +1,21 @@
 pkg_name=libffi
-pkg_version=3.3
+pkg_version=3.4.2
 pkg_origin=core
 pkg_license=('MIT')
 pkg_maintainer="The Habitat Maintainers <humans@habitat.sh>"
-pkg_source=ftp://sourceware.org/pub/${pkg_name}/${pkg_name}-${pkg_version}.tar.gz
+pkg_source=https://github.com/libffi/libffi/releases/download/v${pkg_version}/libffi-${pkg_version}.tar.gz
 pkg_upstream_url=https://sourceware.org/libffi
 pkg_description="The libffi library provides a portable, high level programming interface to various calling conventions.\
   This allows a programmer to call any function specified by a call interface description at run-time."
 pkg_filename=${pkg_name}-${pkg_version}.tar.gz
-pkg_shasum=72fba7922703ddfa7a028d513ac15a85c8d54c8d67f55fa5a4802885dc652056
+pkg_shasum=540fb721619a6aba3bdeef7d940d8e9e0e6d2c193595bc243241b77ff9e93620
 pkg_deps=(core/glibc core/libtool)
-pkg_build_deps=(core/coreutils core/make core/gcc)
+pkg_build_deps=(
+	core/coreutils
+	core/make
+	core/gcc
+)
+
 pkg_lib_dirs=(lib)
 pkg_include_dirs=(include)
 
